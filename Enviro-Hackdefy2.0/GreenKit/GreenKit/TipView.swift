a081bd9298a795f321b22b3dc6d6cf6b9ed114a7
Ryan Bascos <57274168+Involuta@users.noreply.github.com>
2021-03-27 20:08:02 -0700

    added app

---
+++ Enviro-Hackdefy2.0/GreenKit/GreenKit/TipView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GreenKit
+//
+//  Created by Ryan Bascos on 3/27/21.
+//
+
+import SwiftUI
+
+struct TipView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TipView()
+    }
+}
